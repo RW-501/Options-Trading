d00f03ed2b9ff3cc6893ab44139a3775615728b8
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-06-25 16:32:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,98 @@
-/* Reset default browser styles */
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+// Assuming you have a function to make the API call and handle the response
+function fetchOptionsData(priceRange) {
+  const apiKey = 'YOUR_API_KEY';
+  const apiUrl = `https://www.alphavantage.co/api/v1/options/...`; // API URL with the necessary parameters
+
+  // Make the API call using fetch or any other library
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Handle the API response data
+      const optionsData = data['YOUR_DATA_KEY']; // Adjust 'YOUR_DATA_KEY' based on the actual data structure
+
+      // Extract relevant information from the options data
+      const extractedData = optionsData.map(option => ({
+        symbol: option['symbol'],
+        strikePrice: option['strikePrice'],
+        expirationDate: option['expirationDate'],
+        // Add other relevant parameters as needed
+      }));
+
+      // Store the extracted data in the local storage
+      localStorage.setItem('optionsData', JSON.stringify(extractedData));
+    })
+    .catch(error => {
+      // Handle any errors that occurred during the API call
+      console.log('Error fetching options data:', error);
+    });
 }
 
-/* Global styles */
-body {
-  font-family: Arial, sans-serif;
-  line-height: 1.5;
+
+// Assuming you have the options data stored in the local storage
+const optionsData = JSON.parse(localStorage.getItem('optionsData'));
+
+// Function to update the displayed options based on sorting and filtering
+function updateOptionsDisplay() {
+  const sortBy = document.getElementById('sort-by').value;
+  const filterByStrategy = document.getElementById('filter-by-strategy').value;
+
+  let filteredOptions = optionsData;
+
+  // Filter options based on selected strategy
+  if (filterByStrategy !== 'all') {
+    filteredOptions = filteredOptions.filter(option => option.strategy === filterByStrategy);
+  }
+
+  // Sort options based on selected criteria
+  switch (sortBy) {
+    case 'profitability':
+      filteredOptions.sort((a, b) => a.profitability - b.profitability);
+      break;
+    case 'trade-strategies':
+      filteredOptions.sort((a, b) => a.strategy.localeCompare(b.strategy));
+      break;
+    case 'total-cost':
+      filteredOptions.sort((a, b) => a.totalCost - b.totalCost);
+      break;
+    // Add additional cases for other sorting criteria if needed
+  }
+
+  // Update the displayed options
+  const recommendationsList = document.getElementById('recommendations-list');
+  recommendationsList.innerHTML = '';
+
+  filteredOptions.forEach(option => {
+    const optionElement = document.createElement('div');
+    optionElement.textContent = `Symbol: ${option.symbol}, Strike Price: ${option.strikePrice}, Expiration Date: ${option.expirationDate}`;
+    recommendationsList.appendChild(optionElement);
+  });
 }
 
-.container {
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 20px;
-}
+// Add event listeners to the sorting and filtering elements
+document.getElementById('sort-by').addEventListener('change', updateOptionsDisplay);
+document.getElementById('filter-by-strategy').addEventListener('change', updateOptionsDisplay);
 
-/* Header styles */
-header {
-  background-color: #333;
-  color: #fff;
-  padding: 20px;
-}
+// Initial update of options display
+updateOptionsDisplay();
 
-header h1 {
-  font-size: 24px;
-}
 
-/* Main content styles */
-main {
-  margin-top: 20px;
-}
 
-section {
-  margin-bottom: 20px;
-}
+// Show tips pop-up
+document.getElementById('show-tips-btn').addEventListener('click', function () {
+  document.getElementById('tips-popup').style.display = 'block';
+});
 
-h2 {
-  font-size: 20px;
-  margin-bottom: 10px;
-}
+// Close tips pop-up
+document.getElementById('close-tips-btn').addEventListener('click', function () {
+  document.getElementById('tips-popup').style.display = 'none';
+});
 
-form {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
-}
+// Show simulation pop-up
+document.getElementById('show-simulation-btn').addEventListener('click', function () {
+  document.getElementById('simulation-popup').style.display = 'block';
+});
 
-label {
-  display: block;
-}
-
-input[type="number"],
-button {
-  padding: 5px;
-  font-size: 16px;
-}
-
-button {
-  background-color: #333;
-  color: #fff;
-  border: none;
-  cursor: pointer;
-}
-
-#recommendations-list {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  gap: 10px;
-}
-
-#recommendations-list > div {
-  border: 1px solid #ccc;
-  padding: 10px;
-}
-
-#strategies-list {
-  list-style: none;
-}
-
-/* Styles for sorting and filtering section */
-#sorting-section select {
-  padding: 5px;
-  font-size: 16px;
-}
-
-/* Styles for tips and simulations section */
-#tips-section p {
-  margin-bottom: 10px;
-}
-
+// Close simulation pop-up
+document.getElementById('close-simulation-btn').addEventListener('click', function () {
+  document.getElementById('simulation-popup').style.display = 'none';
+});
